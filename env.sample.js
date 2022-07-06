3a44e861bcb2bf7f5e617c01765a4abae8c6be8f
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-06 23:13:49 +0800

    setup global env variables for API config

---
+++ env.sample.js
@@ -0,0 +1,11 @@
+/**
+ * @note
+ *  Please make 'env.js' in the root directory
+ *  and copy the contents here to setup your
+ *  configurations
+ */
+export default {
+  api: {
+    url: 'API_BASE_URL', // ex. https://myapi.com
+  },
+};
