b4bacd5663d09a4b8d37873965dc91a59c06acf3
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-10 15:07:09 +0800

    fixed leftsidebar doing unnecessary calls to api

--- src/layouts/LeftSidebar/LeftSidebar.hook.tsx
+++ src/layouts/LeftSidebar/LeftSidebar.hook.tsx
@@ -1,23 +1,15 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '@services/store';
 
-import ProjectsActions from '@services/actions/projects';
-import { setProjects } from '@services/reducers/projects';
-
 const useLeftSidebar = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    getAllProjects();
+    
   }, []);
 
-  const getAllProjects = async () : Promise<void> => {
-    try {
-      const projects = await ProjectsActions.getAll();
-      dispatch(setProjects(projects));
-    } catch(error) {
-      console.log('getAllProjectsError', error);
-    }
+  return {
+
   }
 }
 
