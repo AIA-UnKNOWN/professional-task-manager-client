ac302a45fe278f18616a125384172470c8df247f
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-22 02:00:37 +0800

    implemented action dropdown in a task edit mode

--- src/layouts/MainContent/components/Tasks/components/Task/components/TaskEdit/components/TaskActions/TaskActions.tsx
+++ src/layouts/MainContent/components/Tasks/components/Task/components/TaskEdit/components/TaskActions/TaskActions.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { FaEllipsisH } from 'react-icons/fa';
 
+import ActionDropdown from '@common/ui/ActionDropdown';
+
 const TaskActions: React.FC = () => {
   return (
     <div>
-      <button>
-        <FaEllipsisH />
-      </button>
+      <ActionDropdown
+        displayedContentComponent={
+          <button
+            className="hover:bg-light-gray-3 p-1 rounded-full"
+          >
+            <FaEllipsisH />
+          </button>
+        }
+        dropdownComponent={
+          <ul className="bg-light-gray rounded-md">
+            <li className="text-center p-1 px-2 text-[11px]
+              rounded-md hover:bg-light-gray-3 cursor-pointer"
+            >
+              Delete
+            </li>
+          </ul>
+        }
+      />
     </div>
   );
 }
