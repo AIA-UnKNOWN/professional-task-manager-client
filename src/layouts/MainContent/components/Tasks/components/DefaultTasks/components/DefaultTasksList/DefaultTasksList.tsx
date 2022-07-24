83bb50392fae57d9c1a4f4820a910d335d1f40ff
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-24 23:57:50 +0800

    group tasks based on their created date

--- src/layouts/MainContent/components/Tasks/components/DefaultTasks/components/DefaultTasksList/DefaultTasksList.tsx
+++ src/layouts/MainContent/components/Tasks/components/DefaultTasks/components/DefaultTasksList/DefaultTasksList.tsx
@@ -7,14 +7,47 @@ const PendingTasksList: React.FC<{
 }> = ({
   tasks,
 }) => {
+  let taskCreatedDates: Array<{ date: string, taskId: number }> = [];
+  
   return (
     <div className="mt-5">
-      {tasks.map((task: TaskInterface) => (
-        <Task
-          key={task.id}
-          data={task}
-        />
-      ))}
+      {tasks.map((task: TaskInterface) => {
+        const MONTH_NAMES = [
+          'January', 'February', 'March', 'April',
+          'May', 'June', 'July', 'August',
+          'September', 'October', 'November', 'December'
+        ];
+
+        const year = new Date(task.createdAt).getFullYear();
+        const month = new Date(task.createdAt).getMonth();
+        const date = new Date(task.createdAt).getDate();
+        let createdDate = `${MONTH_NAMES[month]} ${date}, ${year}`;
+        
+        const taskCreatedDate = {
+          date: createdDate,
+          taskId: task.id,
+        };
+
+        const taskCreatedDatesWithoutTaskIds = taskCreatedDates.map(tcd => tcd.date);
+        if (!taskCreatedDatesWithoutTaskIds.includes(taskCreatedDate.date)) {
+          taskCreatedDates.push(taskCreatedDate);
+        }
+
+        return (
+          <>
+            {taskCreatedDates.map(tcd => tcd.taskId).includes(task.id) && (
+              <div>
+                <span>{taskCreatedDate.date}</span>
+                <hr />
+              </div>
+            )}
+            <Task
+              key={task.id}
+              data={task}
+            />
+          </>
+        )}
+      )}
     </div>
   );
 }
