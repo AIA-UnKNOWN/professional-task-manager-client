fe0819bc63fa389d6d709c0a7b021b71df4dae71
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-31 09:22:53 +0000

    newly created project now reflects immediately

--- src/layouts/LeftSidebar/components/Projects/Projects.hook.tsx
+++ src/layouts/LeftSidebar/components/Projects/Projects.hook.tsx
@@ -1,12 +1,15 @@
 import ProjectActions from '@services/actions/projects';
 import { useAppDispatch, useAppSelector } from "@services/store";
+import { setProjects } from '@services/reducers/projects';
 
 const useProjects = () => {
+  const [ projects ] = useAppSelector(state => [ state.projects ]);
+  const dispatch = useAppDispatch();
   
   const createProject = async (project) => {
     const { status, data } = await ProjectActions.create(project);
     if (status !== 201) return;
-    console.log(data)
+    dispatch(setProjects([ ...projects.data, data.createdProject ]));
   }
 
   return {
