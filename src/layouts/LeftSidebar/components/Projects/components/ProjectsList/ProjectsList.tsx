830a3d61ff2fbf07a612d9ed21bc09e8ea651599
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-31 14:42:35 +0000

    created button for deleting a project

--- src/layouts/LeftSidebar/components/Projects/components/ProjectsList/ProjectsList.tsx
+++ src/layouts/LeftSidebar/components/Projects/components/ProjectsList/ProjectsList.tsx
@@ -1,3 +1,4 @@
+import './ProjectsList.css';
 import React from 'react';
 
 import useProjectsList from './ProjectsList.hook';
@@ -14,19 +15,34 @@ const ProjectsList: React.FC = () => {
   return (
     <div className="my-2">
       {projects.map(project => (
-        <div className={`${project.id === selectedProjectId ? 'bg-light-gray-3' : 'bg-light-gray'}
-          rounded-[10px] text-[15px] w-[211px] h-[34px]
-          flex justify-between items-center px-4 mb-1 cursor-pointer
-          last-of-type:mb-0 hover:bg-light-gray-3 ${project.id === selectedProjectId &&`bg-light-gray-3`}`}
+        <div
           key={project.id}
-          onClick={() => selectProjectById(project.id)}
+          className='project mb-1 last-of-type:mb-0 relative'
         >
-          <span>
-            {project.name}
-          </span>
-          <span className="font-bold">
-            0
-          </span>
+          <div
+            className={`${project.id === selectedProjectId ? 'bg-light-gray-3' : 'bg-light-gray'}
+              rounded-[10px] text-[15px] w-[211px] h-[34px] flex justify-between items-center px-4 cursor-pointer
+              hover:bg-light-gray-3 ${project.id === selectedProjectId &&`bg-light-gray-3`}
+            `}
+            onClick={() => selectProjectById(project.id)}
+          >
+            <span>
+              {project.name}
+            </span>
+            <span className="font-bold">
+              0
+            </span>
+          </div>
+          <div
+            className={`delete-button absolute top-[50%] left-[100%] -translate-y-[50%]`}
+          >
+            <button
+              className='bg-red text-white px-2 py-1 rounded-md text-[14px] ml-2'
+              onClick={() => console.log('delete', project.id)}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       ))}
     </div>
