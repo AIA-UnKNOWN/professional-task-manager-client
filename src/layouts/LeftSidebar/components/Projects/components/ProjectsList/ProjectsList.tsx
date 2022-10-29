a3417669d2d9348b95e98b0fbd0d35b6f880c6b6
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-29 05:28:08 -0400

    added pending tasks count besides project labels

--- src/layouts/LeftSidebar/components/Projects/components/ProjectsList/ProjectsList.tsx
+++ src/layouts/LeftSidebar/components/Projects/components/ProjectsList/ProjectsList.tsx
@@ -1,5 +1,6 @@
 import './ProjectsList.css';
 import React from 'react';
+import { useAppSelector } from '@services/store';
 
 import useProjectsList from './ProjectsList.hook';
 import { ProjectsListInterface } from 'constants/interfaces';
@@ -12,42 +13,46 @@ const ProjectsList: React.FC<ProjectsListInterface> = ({
     selectedProjectId,
     selectProjectById,
   } = useProjectsList();
+  const [tasks] = useAppSelector(state => [state.tasks]);
 
   if (projects && projects.length === 0) return null;
 
   return (
     <div className="my-2">
-      {projects.map(project => (
-        <div
-          key={project.id}
-          className='project mb-1 last-of-type:mb-0 relative'
-        >
+      {projects.map(project => {
+        const pendingTasksCount = tasks?.data?.filter(task => task.project_id === project.id && !task.is_completed)?.length || 0;
+        return (
           <div
-            className={`${project.id === selectedProjectId ? 'bg-light-gray-3' : 'bg-light-gray'}
-              rounded-[10px] text-[15px] w-[211px] h-[34px] flex justify-between items-center px-4 cursor-pointer
-              hover:bg-light-gray-3 ${project.id === selectedProjectId &&`bg-light-gray-3`}
-            `}
-            onClick={() => selectProjectById(project.id)}
+            key={project.id}
+            className='project mb-1 last-of-type:mb-0 relative'
           >
-            <span>
-              {project.name}
-            </span>
-            <span className="font-bold">
-              0
-            </span>
-          </div>
-          <div
-            className={`delete-button absolute top-[50%] left-[100%] -translate-y-[50%]`}
-          >
-            <button
-              className='bg-red text-white px-2 py-1 rounded-md text-[14px] ml-2'
-              onClick={() => onDeleteProject(project.id)}
+            <div
+              className={`${project.id === selectedProjectId ? 'bg-light-gray-3' : 'bg-light-gray'}
+                rounded-[10px] text-[15px] w-[211px] h-[34px] flex justify-between items-center px-4 cursor-pointer
+                hover:bg-light-gray-3 ${project.id === selectedProjectId &&`bg-light-gray-3`}
+              `}
+              onClick={() => selectProjectById(project.id)}
             >
-              Delete
-            </button>
+              <span>
+                {project.name}
+              </span>
+              <span className="font-bold">
+                {pendingTasksCount}
+              </span>
+            </div>
+            <div
+              className={`delete-button absolute top-[50%] left-[100%] -translate-y-[50%]`}
+            >
+              <button
+                className='bg-red text-white px-2 py-1 rounded-md text-[14px] ml-2'
+                onClick={() => onDeleteProject(project.id)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   );
 }
