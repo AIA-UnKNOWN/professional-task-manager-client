564c46a6841ebeafb56015408713c44f5323313b
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-02-25 20:05:22 +0800

    fixed issue on labels count not updating when a task label is updated

--- src/layouts/MainContent/components/Tasks/components/Task/components/TaskEdit/TaskEdit.hook.tsx
+++ src/layouts/MainContent/components/Tasks/components/Task/components/TaskEdit/TaskEdit.hook.tsx
@@ -2,10 +2,17 @@ import { useAppDispatch, useAppSelector } from '@services/store';
 
 import TaskActions from '@services/actions/tasks';
 import { setTasks } from '@services/reducers/tasks';
+import { setLabels } from '@services/reducers/labels';
 
 const useTaskEdit = () => {
   const dispatch = useAppDispatch();
-  const [tasks] = useAppSelector(state => [state.tasks]);
+  const [
+    tasks,
+    labels,
+  ] = useAppSelector(state => [
+    state.tasks,
+    state.labels,
+  ]);
 
   const setTaskLabelByLabelId = async (labelId, task) => {
     const { status } = await TaskActions.update(task.id, { ...task, label_id: labelId });
@@ -13,6 +20,16 @@ const useTaskEdit = () => {
     dispatch(setTasks(tasks.data.map(reduxTask => {
       if (reduxTask.id !== task.id) return reduxTask;
       return { ...reduxTask, label_id: labelId }
+    })));
+    dispatch(setLabels(labels.data.map(label => {
+      if (label.id !== labelId) return label;
+      const labelTaskIndex = label.tasks.findIndex(labelTask => labelTask.id === task.id);
+      return {
+        ...label,
+        tasks: labelTaskIndex === -1 ?
+          [...label.tasks, task]
+          : label.tasks,
+      }
     })));
   }
 
